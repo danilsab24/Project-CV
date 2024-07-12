3a27b680f77f1c62818bdd0c3b1e46a485baa605
danilsab24 <sabatini.1890300@studenti.uniroma1.it>
2024-07-12 15:28:59 +0200

    test with videocamera

---
+++ model_CNN.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+class HandGestureCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(HandGestureCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(128 * 28 * 28, 512)
+        self.fc2 = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        x = self.pool(nn.ReLU()(self.conv1(x)))
+        x = self.pool(nn.ReLU()(self.conv2(x)))
+        x = self.pool(nn.ReLU()(self.conv3(x)))
+        x = x.view(-1, 128 * 28 * 28)  # Flatten the tensor
+        x = nn.ReLU()(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+    
+num_classes = 29
+model = HandGestureCNN(num_classes=num_classes)
